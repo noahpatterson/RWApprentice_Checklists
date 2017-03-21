2191cb88785c741df5c1e8fa03f805f85d842c8b
noahpatterson <n@noahpatterson.me>
2017-03-21 15:06:17 -0600

    Test out a local notification

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,6 +22,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let controller           = navigationController.viewControllers[0] as! AllListsViewController
         
         controller.dataModel = dataModel
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Hello!"
+        content.body  = "I am a local notification"
+        content.sound = UNNotificationSound.default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: "MyNotification", content: content, trigger: trigger)
+        
+        let center = UNUserNotificationCenter.current()
+        center.add(request)
+        center.requestAuthorization(options: [.alert, .sound]) {
+            granted, error in
+            if granted {
+                print("we have permission")
+            } else {
+                print("permission denited")
+            }
+        }
         return true
     }
 
